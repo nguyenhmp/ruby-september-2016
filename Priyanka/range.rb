b58c2a0c5a7c4add394820bab3d2a767b39ba016
Priyanka <priyanka3007@gmail.com>
2016-09-06 23:27:34 -0400

    Basic assignments

---
+++ Priyanka/range.rb
@@ -0,0 +1,17 @@
+y= ('a'..'z')
+# puts "the first letter is " +y.min.to_s
+# puts "the last letter is " +y.max.to_s
+# puts y.to_a.sort.to_s
+
+puts y.member?'a'
+
+puts y.include?'zz'
+
+puts y.last
+
+puts y.max
+
+puts y.min
+
+
+
