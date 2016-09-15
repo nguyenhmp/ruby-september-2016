d6b9d9be59a7af456d0abd91f9e58fdc19e63352
Phil Boothe <pfb3cn@virginia.edu>
2016-09-15 12:45:37 -0400

    added Random Number Generator to timedisplay project

--- phil/rails/timedisplay/config/routes.rb
+++ phil/rails/timedisplay/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-    root 'times#main'
+    get 'generator' => 'generators#index'
+    post 'generator' => 'generators#create'
+
+    get 'time' => 'times#main'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
